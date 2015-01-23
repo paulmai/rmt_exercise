8682584438f6a7af11f7102a689307250870ee0b
Paul Maidment <paulmaidmentuk@googlemail.com>
2015-01-23 11:29:41 +0000

    Correct formatting of prices

--- lib/PaulMaidment/ShoppingCart/CartImpl.php
+++ lib/PaulMaidment/ShoppingCart/CartImpl.php
@@ -14,13 +14,15 @@
 
 		public function getTotalSum()
 		{
-			$longest = 0;
+			$total = 0;
 			$summary = "\n";
 			foreach($this->products as $product){
 
+				$line_price = $product['obj']->getTotalPrice($product['qty']);
 				$qty_col = str_pad($product['qty'],10,' ');
 				$name_col = str_pad($product['obj']->getName(),20,' ');
-				$price_col = str_pad($product['obj']->getTotalPrice($product['qty']),10,' ');
+				$price_col = str_pad($line_price,10,' ');
+				$total += $line_price;
 				$separator = "\n";
 				$line = $qty_col.$name_col.$price_col.$separator;
 				$summary .= $line;
@@ -29,7 +31,9 @@
 			$summary .= str_pad("",40,"=");
 
 			$summary .= "\n";
-			$summary .= "Total\t\t\t";
+			$summary .= str_pad('',10,' ');
+			$summary .= str_pad('Total',20,' ');
+			$summary .= str_pad(number_format($total,2),10,' ');
 
 			return $summary;
 		}	
